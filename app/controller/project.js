59ad1a6a50509e85ef41da0d3e73eb7f5cc2fff5
lwjmzla <214245088@qq.com>
2022-08-17 00:20:28 +0800

    1

--- app/controller/project.js
+++ app/controller/project.js
@@ -5,7 +5,18 @@ const Controller = require('egg').Controller;
 class ProjectController extends Controller {
   async index() {
     const { ctx } = this;
-    ctx.body = 'hi, egg ProjectController';
+    ctx.body = [
+      {
+        name: 'Vue3-ts模板',
+        npmName: 'lwj-vue3',
+        version: '1.0.0',
+      },
+      {
+        name: 'Vue3 UI 组件库',
+        npmName: 'lwj-ui',
+        version: '1.0.0',
+      },
+    ];
   }
 }
 
--- app/router.js
+++ app/router.js
@@ -6,5 +6,5 @@
 module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
-  router.get('/project', controller.project.index);
+  router.get('/project/template', controller.project.index);
 };
