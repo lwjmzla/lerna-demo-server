6b369f3b6f11d8b90ed3a9f3ba8e14230d511ca0
lwjmzla <214245088@qq.com>
2022-08-20 00:17:46 +0800

    1

--- app/controller/project.js
+++ app/controller/project.js
@@ -10,11 +10,13 @@ class ProjectController extends Controller {
         name: 'Vue3-ts模板',
         npmName: 'lwj-vue3',
         version: 'latest',
+        type: 'normal',
       },
       {
         name: 'Vue3 UI 组件库',
         npmName: 'lwj-ui',
         version: 'latest',
+        type: 'custom',
       },
     ];
   }
